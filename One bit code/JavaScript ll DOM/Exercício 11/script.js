fe42abc05623ee257b8834e281602d91d0e8acd5
Kaio Bruno Barbosa Magalhães <bmkaiobruno@gmail.com>
2022-11-19 17:04:56 -0300

    Studies 18/11

--- One bit code/JavaScript ll DOM/Exercício 11/script.js
+++ One bit code/JavaScript ll DOM/Exercício 11/script.js
@@ -1,36 +1,55 @@
 function add_player() {
-  const inputs_div = document.querySelector("#inputs-div");
+  const input_player_name = document.getElementById("player-name").value;
+  const input_player_number = document.getElementById("player-number").value;
+  const input_player_position =
+    document.getElementById("player-position").value;
 
-  const players_info_title = document.createElement("h2");
-  players_info_title.className = "display-2 mb-5";
-  players_info_title.innerText = "Informações do Jogador";
+  const confirm_button = confirm("As informações do jogador está correta?");
 
-  const input_player_position = document.createElement("input");
-  input_player_position.type = "text";
-  input_player_position.className = "form-control";
-  input_player_position.placeholder = "Posição do Jogador";
+  if (
+    (confirm_button === true && input_player_name !== null) ||
+    input_player_number !== null ||
+    input_player_position !== null
+  ) {
+    const table = document.querySelector("#oficial-team > table > tbody");
+    const tr = document.createElement("tr");
+    tr.id = input_player_number;
 
-  const input_player_name = document.createElement("input");
-  input_player_name.type = "text";
-  input_player_name.className = "form-control mt-3";
-  input_player_name.placeholder = "Nome do Jogador";
+    const th_player_number = document.createElement("th");
+    th_player_number.innerText = input_player_number;
+    th_player_number.setAttribute("scope", "row");
+    th_player_number.className = "text-center";
 
-  const input_player_number = document.createElement("input");
-  input_player_number.type = "number";
-  input_player_number.className = "form-control mt-3";
-  input_player_number.placeholder = "Número do Jogador";
+    const td_player_name = document.createElement("td");
+    td_player_name.innerText = input_player_name;
+    td_player_name.className = "text-center";
 
-  const button_confirm_lineup = document.createElement("button");
-  // button_confirm_lineup.addEventListener("click", confirm_lineup);
-  button_confirm_lineup.type = "button";
-  button_confirm_lineup.className = "btn btn-success mt-3";
-  button_confirm_lineup.innerText = "Escalar";
+    const td_player_position = document.createElement("td");
+    td_player_position.innerText = input_player_position;
+    td_player_position.className = "text-center";
 
-  inputs_div.append(
-    players_info_title,
-    input_player_position,
-    input_player_name,
-    input_player_number,
-    button_confirm_lineup
+    table.appendChild(tr);
+    tr.append(th_player_number, td_player_name, td_player_position);
+  } else if (
+    input_player_name === null ||
+    input_player_number === null ||
+    input_player_position === null
+  ) {
+    alert(
+      "A escalação do jogador foi cancelada, os dados podem está incompletos"
+    );
+  }
+}
+
+function remove_player() {
+  const input_player_number_remove = document.getElementById(
+    "input-player-number-remove"
   );
+  const choosed_player = document.getElementById(
+    input_player_number_remove.value
+  );
+  const tbody = document.querySelector("#oficial-team > table > tbody");
+  // const th = document.querySelector("#oficial-team > table > tbody > th");
+
+  tbody.removeChild(choosed_player);
 }
