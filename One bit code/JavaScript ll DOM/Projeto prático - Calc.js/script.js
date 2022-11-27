c3807821eb543b332c63e7d88a9e642151c6400e
Kaio Bruno Barbosa Magalhães <bmkaiobruno@gmail.com>
2022-11-27 15:56:14 -0300

    Calc project

---
+++ One bit code/JavaScript ll DOM/Projeto prático - Calc.js/script.js
@@ -0,0 +1,11 @@
+// const input = document.querySelector(".input-display");
+
+// document.querySelectorAll(
+//   ".calc-btn",
+//   forEach(function (calc_btn) {
+//     calc_btn.addEventListener("click", function () {
+//       const value = calc_btn.dataset.value;
+//       input.value += value;
+//     });
+//   })
+// );
